9782327dbd677b2d6402f24e00094583399fd016
Ian Foster <Ian Foster>
2022-02-24 10:07:36 -0700

    init

--- Lab4/demo.js
+++ Lab4/demo.js
@@ -10,8 +10,76 @@ function CalculateCommission()
 {
     var topCommission = 0;
     var topSalesPerson = "Nobody yet";
+    var salesperson = ["Noah", "Kody", "Michael", "Connor", "James", "Scott","Mia"];
+    var Jan = [1500, 15000,30000,15000,58000,15000,6000];
+    var Feb = [7000, 5500, 47000, 20000, 29000, 26000, 17000];
+    var Dec = [1500, 15000,40000,18000,45000,28000,9000];
 
-    //Your code goes here
+    var totalsales=[];
+    var commission=[];
+    var avgcommission=[];
+
+    for(var i=0;i<=6;i++)
+{
+totalsales[0] = Jan[0] + Feb[0] + Dec[0];
+totalsales[1] = Jan[1] + Feb[1] + Dec[1];
+totalsales[2] = Jan[2] + Feb[2] + Dec[2];
+totalsales[3] = Jan[3] + Feb[3] + Dec[3];
+totalsales[4] = Jan[4] + Feb[4] + Dec[4];
+totalsales[5] = Jan[5] + Feb[5] + Dec[5];
+totalsales[6] = Jan[6] + Feb[6] + Dec[6];
+
+if(totalsales[i]<=5000){
+
+commission[i]=totalsales[i]*.01;
+
+}
+
+else if(totalsales[i]<=10000){
+
+    commission[i]=totalsales[i]*.02;
+
+    }
+
+    else if(totalsales[i]<=15000)
+
+    {
+    commission[i]=totalsales[i]*.03;
+
+    }
+
+    else if(totalsales[i]<=30000)
+
+    {
+
+    commission[i]=totalsales[i]*.05;
+
+    }
+
+    else
+
+    {
+
+    commission[i]=totalsales[i]*.08;
+
+}
+
+avgcommission[i]=commission[i]/3;
+
+console.log(salesperson[i]+" "+commission[i]+" "+ avgcommission[i]);
+
+if(topCommission<=commission[i]){
+topCommission=commission[i];
+topSalesPerson=salesperson[i];
+}
+
+}
+
+alert("Top salesperson is"+" "+ topSalesPerson+" "+"top commission amount is " +" " +topCommission);
+
+}
+
+//Your code goes here
     /*
     Here's the pseudocode in case you are wondering how to get started:
     1. Save salesperson names in an array
@@ -34,5 +102,3 @@ function CalculateCommission()
         to determine if this is the top commission
     6. Outside the "For" loop, display topCommission and topSalesperson name.
 */
-
-}
