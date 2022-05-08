76685745874a217d353bcb86f2624b075589425b
Ian Foster <Ian Foster>
2022-05-08 14:52:09 -0700

    This didnt push when I pushed it last thursday :/

--- Lab12/game.js
+++ Lab12/game.js
@@ -1,5 +1,44 @@
 $(function(){
     //jQuery code goes here
+    $(function(){
+        //jQuery code goes here
+        $("#up").on("click",function(){
+            $("#box").animate({top: '0px'});
+         });
     
+         $("#down").on("click",function(){
+            var height = $("#container").innerHeight() - $("#box").outerHeight() +"px";
+            $("#box").animate({top: height});
+         });
+    
+         $("#left").on("click",function(){
+            var width = $("#container").innerWidth() - $("#box").outerWidth() +"px";
+            $("#box").animate({left: '0px'});
+         });
+    
+         $("#right").on("click",function(){
+            var width = $("#container").innerWidth() - $("#box").outerWidth() +"px";
+            $("#box").animate({left: width});
+         });
+    
+         $("#fadeout").on("click",function(){
+            $("#box").fadeOut();
+         });
+    
+         $("#fadein").on("click",function(){
+            $("#box").fadeIn();
+         });
+    
+         $("#blink").on("click",function(){
+            $("#box").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+        });
+    
+         $("#reset").on("click",function(){
+            $("#box").animate({top: '0px'});
+            $("#box").animate({left: '0px'});
+            $("#box").fadeIn();
+         });
+         
+        });
     
     });
